cd3c115996a0bbc9b10bdf37128618e014dbd555
Nirmit982 <137621587+Nirmit982@users.noreply.github.com>
2025-04-18 10:31:47 +0530

    Create DBUtil.java

---
+++ DBUtil.java
@@ -0,0 +1,7 @@
+import java.sql.*;
+public class DBUtil {
+    public static Connection getConnection() throws Exception {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/company", "root", "password");
+    }
+}
